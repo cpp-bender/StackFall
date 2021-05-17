2f0a632a7102fb5325cd5f1224b791b5c4623552
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-05-17 12:56:39 +0300

    Initial commit: Assets, PlayerController and PlayerData added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController :MonoBehaviour
+{
+    [SerializeField] private PlayerData playerData;
+
+    private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        playerData.CanHit = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerData.CanHit = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            playerData.CanHit = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (playerData.CanHit)
+        {
+            MoveDown();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!playerData.CanHit)
+        {
+            MoveUp();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!playerData.CanHit)
+        {
+            MoveUp();
+        }
+    }
+
+    private void MoveUp()
+    {
+        body.velocity = Vector3.up * playerData.Speed * Time.deltaTime;
+    }
+
+    private void MoveDown()
+    {
+        body.velocity = Vector3.down * playerData.Speed * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stack Fall/Player/Player Data")]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField] private float speed;
+    [SerializeField] private bool canHit;
+    public float Speed { get => speed; }
+    public bool CanHit { get => canHit; set => canHit = value; }
+}
